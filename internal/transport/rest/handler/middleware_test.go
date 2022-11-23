5b387193fa9f4e30a70502512cc026a69141612d
Aleksander Radovsky <alrad.sys@gmail.com>
2022-11-23 11:33:21 +0300

    Test middleware

---
+++ internal/transport/rest/handler/middleware_test.go
@@ -0,0 +1,147 @@
+package handler
+
+import (
+	"coins-app/internal/service"
+	mock_service "coins-app/internal/service/mocks"
+	"errors"
+	"github.com/gin-gonic/gin"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHandler_userIdentity(t *testing.T) {
+	type mockBehavior func(s *mock_service.MockAuthorization, token string)
+
+	tests := []struct {
+		name                 string
+		headerName           string
+		headerValue          string
+		token                string
+		mockBehavior         mockBehavior
+		expectedStatusCode   int
+		expectedResponseBody string
+	}{
+		{
+			name:        "Ok",
+			headerName:  "Authorization",
+			headerValue: "Bearer token",
+			token:       "token",
+			mockBehavior: func(r *mock_service.MockAuthorization, token string) {
+				r.EXPECT().ParseToken(token).Return(1, nil)
+			},
+			expectedStatusCode:   200,
+			expectedResponseBody: "1",
+		},
+		{
+			name:                 "Invalid Header Name",
+			headerName:           "",
+			headerValue:          "Bearer token",
+			token:                "token",
+			mockBehavior:         func(r *mock_service.MockAuthorization, token string) {},
+			expectedStatusCode:   401,
+			expectedResponseBody: `{"error":"empty auth header"}`,
+		},
+		{
+			name:                 "Invalid Header Value",
+			headerName:           "Authorization",
+			headerValue:          "Bearr token",
+			token:                "token",
+			mockBehavior:         func(r *mock_service.MockAuthorization, token string) {},
+			expectedStatusCode:   401,
+			expectedResponseBody: `{"error":"invalid auth header"}`,
+		},
+		{
+			name:                 "Empty Token",
+			headerName:           "Authorization",
+			headerValue:          "Bearer ",
+			token:                "token",
+			mockBehavior:         func(r *mock_service.MockAuthorization, token string) {},
+			expectedStatusCode:   401,
+			expectedResponseBody: `{"error":"token is empty"}`,
+		},
+		{
+			name:        "Parse Error",
+			headerName:  "Authorization",
+			headerValue: "Bearer token",
+			token:       "token",
+			mockBehavior: func(r *mock_service.MockAuthorization, token string) {
+				r.EXPECT().ParseToken(token).Return(0, errors.New("invalid token"))
+			},
+			expectedStatusCode:   401,
+			expectedResponseBody: `{"error":"invalid token"}`,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			// Init Dependencies
+			c := gomock.NewController(t)
+			defer c.Finish()
+
+			repo := mock_service.NewMockAuthorization(c)
+			test.mockBehavior(repo, test.token)
+
+			services := &service.Service{Authorization: repo}
+			handler := Handler{services}
+
+			// Init Endpoint
+			r := gin.New()
+			r.GET("/identity", handler.userIdentity, func(c *gin.Context) {
+				id, _ := c.Get(userCtx)
+				c.String(200, "%d", id)
+			})
+
+			// Init Test Request
+			w := httptest.NewRecorder()
+			req := httptest.NewRequest("GET", "/identity", nil)
+			req.Header.Set(test.headerName, test.headerValue)
+
+			r.ServeHTTP(w, req)
+
+			// Asserts
+			assert.Equal(t, w.Code, test.expectedStatusCode)
+			assert.Equal(t, w.Body.String(), test.expectedResponseBody)
+		})
+	}
+}
+
+func TestGetUserId(t *testing.T) {
+	var getContext = func(id int) *gin.Context {
+		ctx := &gin.Context{}
+		ctx.Set(userCtx, id)
+		return ctx
+	}
+
+	tests := []struct {
+		name       string
+		ctx        *gin.Context
+		id         int
+		shouldFail bool
+	}{
+		{
+			name: "Ok",
+			ctx:  getContext(1),
+			id:   1,
+		},
+		{
+			ctx:        &gin.Context{},
+			name:       "Empty",
+			shouldFail: true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			id, err := getUserId(test.ctx)
+			if test.shouldFail {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+
+			assert.Equal(t, id, test.id)
+		})
+	}
+}
